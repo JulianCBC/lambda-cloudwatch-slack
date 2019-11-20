0af406f72fdb05b07edc499f613ddff2d088a890
Julian Calaby <julian@cabinetsbycomputer.com>
2019-11-20 12:11:47 +1100

    Add trivial endpoint testing using Pact's mock service

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,13 +1,20 @@
-NODE_LAMBDA=./node_modules/node-lambda/bin/node-lambda
+set -x
 
-$NODE_LAMBDA run -x test/context.json -j test/sns-codepipeline-event-pipeline-started.json
-$NODE_LAMBDA run -x test/context.json -j test/sns-codepipeline-event-stage-started.json
-$NODE_LAMBDA run -x test/context.json -j test/sns-codepipeline-event-stage-succeeded.json
-$NODE_LAMBDA run -x test/context.json -j test/sns-codepipeline-event-stage-failed.json
-$NODE_LAMBDA run -x test/context.json -j test/sns-cloudwatch-event.json
-$NODE_LAMBDA run -x test/context.json -j test/sns-event.json
-$NODE_LAMBDA run -x test/context.json -j test/sns-elastic-beanstalk-event.json
-$NODE_LAMBDA run -x test/context.json -j test/sns-codedeploy-event.json
-$NODE_LAMBDA run -x test/context.json -j test/sns-codedeploy-configuration.json
-$NODE_LAMBDA run -x test/context.json -j test/sns-elasticache-event.json
-$NODE_LAMBDA run -x test/context.json -j test/sns-autoscaling-event.json
+pact-mock-service start -p 9000 --ssl
+
+export UNENCRYPTED_HOOK_URL="https://localhost:9000/"
+export NODE_TLS_REJECT_UNAUTHORIZED=0
+
+node-lambda run -x test/context.json -j test/sns-codepipeline-event-pipeline-started.json
+node-lambda run -x test/context.json -j test/sns-codepipeline-event-stage-started.json
+node-lambda run -x test/context.json -j test/sns-codepipeline-event-stage-succeeded.json
+node-lambda run -x test/context.json -j test/sns-codepipeline-event-stage-failed.json
+node-lambda run -x test/context.json -j test/sns-cloudwatch-event.json
+node-lambda run -x test/context.json -j test/sns-event.json
+node-lambda run -x test/context.json -j test/sns-elastic-beanstalk-event.json
+node-lambda run -x test/context.json -j test/sns-codedeploy-event.json
+node-lambda run -x test/context.json -j test/sns-codedeploy-configuration.json
+node-lambda run -x test/context.json -j test/sns-elasticache-event.json
+node-lambda run -x test/context.json -j test/sns-autoscaling-event.json
+
+pact-mock-service stop -p 9000
