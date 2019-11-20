c5165b4ed2f2c9bb0f43e3ed7b7a97be6246354a
Julian Calaby <julian@cabinetsbycomputer.com>
2019-11-20 12:15:55 +1100

    Make testing more dynamic

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,16 +5,10 @@ pact-mock-service start -p 9000 --ssl
 export UNENCRYPTED_HOOK_URL="https://localhost:9000/"
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
-node-lambda run -x test/context.json -j test/sns-codepipeline-event-pipeline-started.json
-node-lambda run -x test/context.json -j test/sns-codepipeline-event-stage-started.json
-node-lambda run -x test/context.json -j test/sns-codepipeline-event-stage-succeeded.json
-node-lambda run -x test/context.json -j test/sns-codepipeline-event-stage-failed.json
-node-lambda run -x test/context.json -j test/sns-cloudwatch-event.json
-node-lambda run -x test/context.json -j test/sns-event.json
-node-lambda run -x test/context.json -j test/sns-elastic-beanstalk-event.json
-node-lambda run -x test/context.json -j test/sns-codedeploy-event.json
-node-lambda run -x test/context.json -j test/sns-codedeploy-configuration.json
-node-lambda run -x test/context.json -j test/sns-elasticache-event.json
-node-lambda run -x test/context.json -j test/sns-autoscaling-event.json
+for i in test/*.test.json; do
+	echo "Testing $i ..."
+	node-lambda run -x test/context.json -j $i
+	echo
+done
 
 pact-mock-service stop -p 9000
